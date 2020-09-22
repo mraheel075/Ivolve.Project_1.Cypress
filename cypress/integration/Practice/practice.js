960fe058d86317f4b2c02a80721bda8b75b8e6b4
Muhammad Raheel <mraheel075@gmail.com>
2020-09-22 16:23:37 +0500

    thrid commit

--- cypress/integration/Practice/practice.js
+++ cypress/integration/Practice/practice.js
@@ -6,6 +6,12 @@ describe('ver', () => {
             cy.visit('https://www.globalsqa.com/samplepagetest/');
             cy.get('#g2599-name').type("abcsdfsd");
             cy.get('#g2599-email').type('abc@gmail.com')
+
+        
+
+
+         const yourFixturePath = 'data.json';
+         cy.get('[data-cy="file-input"]').attachFile(yourFixturePath);
         
 
     })
